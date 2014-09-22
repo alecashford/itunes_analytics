34c64cb2495d1de7c204e9e1356e07e94988b5b0
Alec Ashford <ashford91@gmail.com>
2014-09-22 16:31:25 -0700

    improved algorithm, added rank

--- itunes.py
+++ itunes.py
@@ -1,13 +1,14 @@
 import plistlib
 import requests
+import datetime
 from urllib import urlencode
-
-print urlencode(payload)
 
 tracks = plistlib.readPlist('/Users/aashford/Desktop/Library.xml')['Tracks']
 songs = [tracks[i] for i in tracks if tracks[i].get('Genre', None) not in ('Podcast', None)]
 
 play_counts = {}
+ages = {}
+song_ranks = {}
 
 for song in songs:
 	if 'Artist' in song:
@@ -16,16 +17,36 @@ for song in songs:
 		else:
 			play_counts[song['Artist']] += song.get('Play Count', 0)
 
-new_list = [{'name': key, 'play_count': value} for key, value in play_counts.iteritems()]
+for song in songs:
+	if 'Artist' in song:
 
-sorted_list = sorted(new_list, key = lambda k: k['play_count'], reverse = True)
+		age_of_song = (datetime.datetime.now() - song['Date Added']).total_seconds()
+		
+		if song['Artist'] not in ages:
+			ages[song['Artist']] = age_of_song
+		else:
+			ages[song['Artist']] = max(ages[song['Artist']], age_of_song)
 
-for i in sorted_list[:100]:
+for k, v in play_counts.iteritems():
+	song_ranks[k] = ages[k] / max(v, 1)
+
+
+counts_as_list = [{'name': key, 'play_count': value} for key, value in play_counts.iteritems()]
+
+ranks_as_list = [{'name': key, 'rank': value} for key, value in song_ranks.iteritems()]
+
+
+sorted_count_list = sorted(counts_as_list, key = lambda k: k['play_count'], reverse = True)
+
+sorted_rank_list = sorted(ranks_as_list, key = lambda k: k['rank'])
+
+
+
+for i in sorted_count_list[:100]:
 	print i['name']
 
-
-
-
+for i in sorted_rank_list[:100]:
+	print i['name']
 
 
 
